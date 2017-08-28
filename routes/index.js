67e3295ff307b0b694ae3786e9a6c6d2cdee8b22
oToToT <ty1208chiang@gmail.com>
2017-08-28 10:53:02 +0800

    add front-end

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,19 @@ var router = express.Router();
 var announce = require('../models/announce');
 
 router.get('/', function(req, res, next) {
-   announce.
-      find({}).
-      sort({'date': -1}).
-      limit(10).
-      exec(function(err, anns) {
-      res.render('index', {announce: anns, title: "Express"});   
-   });
+    announce.
+    find({}).
+    sort({ 'date': -1 }).
+    limit(10).
+    exec(function(err, anns) {
+        res.render('index', { announce: anns, title: "Express" });
+    });
+});
+router.get('/about', function(req, res, next) {
+    res.render('about');
+});
+router.get('/detail', function(req, res, next) {
+    res.render('detail');
 });
 
 module.exports = router;
