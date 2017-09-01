ac04a62c0c9d27d2de9daa2c3dbfaa2559933f46
oToToT <ty1208chiang@gmail.com>
2017-09-01 15:52:08 +0800

    done querying news.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var announce = require('../models/announce');
+var url = require('url');
+var moment = require('moment');
 
 router.get('/', function(req, res, next) {
     announce.
@@ -15,22 +17,27 @@ router.get('/about', function(req, res, next) {
     res.render('about');
 });
 router.get('/detail', function(req, res, next) {
+    var queryData = url.parse(req.url, true).query; 
+    console.log(queryData);
+    if(!queryData.id) return res.redirect("/news");
     announce.
-    find({}).
-    sort({ 'date': -1 }).
-    exec(function(err, anns) {
-        res.render('detail', { announce: anns });
+    findOne({_id: queryData.id}).
+    exec(function(err, ann) {
+        var curDay = new Date();
+        if(ann.date > curDay) res.redirect("/news");
+        else res.render('detail', { announce: ann, moment: moment });
     });
 });
 router.get('/qna', function(req, res, next) {
     res.render('qna');
 });
 router.get('/news', function(req, res, next) {
+    var curDate = new Date();
     announce.
-    find({}).
+    find({date: {$lte: curDate} }).
     sort({ 'date': -1 }).
     exec(function(err, anns) {
-        res.render('news', { announce: anns });
+        res.render('news', { announce: anns, moment: moment });
     });
 });
 router.get('/rule', function(req, res, next) {
